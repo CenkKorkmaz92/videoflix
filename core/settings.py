46eec7716d04e50b164dce2ccb06348d9c0e1df5
Cenk Korkmaz <Cenk.Korkmaz92@gmail.com>
2025-07-15 13:20:50 +0200

    Remove STATICFILES_DIRS to prevent static files warnings for all users

--- core/settings.py
+++ core/settings.py
@@ -168,10 +168,6 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "static"
 
-# Additional locations for static files during development
-STATICFILES_DIRS = [
-    BASE_DIR / "staticfiles",
-]
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
