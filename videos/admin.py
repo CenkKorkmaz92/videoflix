5fe565738498946fb83cd4469df88533473ff4e2
Cenk Korkmaz <Cenk.Korkmaz92@gmail.com>
2025-07-15 20:21:19 +0200

    slight chnages to admin panel

--- videos/admin.py
+++ videos/admin.py
@@ -3,6 +3,6 @@ from .models import Video
 
 @admin.register(Video)
 class VideoAdmin(admin.ModelAdmin):
-    list_display = ("title", "owner", "created_at", "is_processed")
-    search_fields = ("title", "owner__email")
+    list_display = ("title", "uploaded_by", "created_at", "is_processed")
+    search_fields = ("title", "uploaded_by__email")
     ordering = ("-created_at",)
